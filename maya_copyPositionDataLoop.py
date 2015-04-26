af5340cd9125b65b772491166bef25119a9c902f
mccuemandan <mccuemandan@gmail.com>
2015-04-26 00:22:00 -0700

    maya_copyPositionDataLoop
    fixed indenting issues and constraint variable

--- maya_copyPositionDataLoop.py
+++ maya_copyPositionDataLoop.py
@@ -1,12 +1,12 @@
 import maya.cmds as cmds
 
-quantity = 25
+quantity = 10
 
 # Parent Parameters:
 parentPrefix = "parentObj"
 parentStartNum = 1
 parentSuffix = ""
-onlyParent = False		# True if parenting multiple things to single obj
+onlyParent = False      # True if parenting multiple things to single obj
 
 # Child Parameters:
 childPrefix = "childObj"
@@ -15,13 +15,13 @@ childSuffix = ""
 
 # Point Constraint Parameters:
 pointWeight = 1.0 # default is 1.0
-skipPosition = []	# "x", "y", "z", or "none". "none" is default
+skipPosition = []   # "x", "y", "z", or "none". "none" is default
 
 # Orient Constraint Parameters:
 orientWeight = 1.0
-skipRotation= []	# "x", "y", "z", or "none". "none" is default
+skipRotation= []    # "x", "y", "z", or "none". "none" is default
 
-# define loop parameterst
+# define loop parameters:
 parentNum = parentStartNum
 childNum = childStartNum
 
@@ -31,13 +31,13 @@ for i in range(quantity):
 
     # Copy Position Data
     pointConstraintName = str(currentChild) + "_pointConstraint1"
-    cmds.pointConstraint( currentParent, currentChild, w= pointWeight, sk= skipPosition, name= constraintName )
+    cmds.pointConstraint( currentParent, currentChild, w= pointWeight, sk= skipPosition, name= pointConstraintName )
     cmds.delete(pointConstraintName)
 
-	  # Copy Rotation Data
-	  orientConstraintName = str(currentChild) + "_orientConstraint1"
-	  cmds.orientConstraint( currentParent, currentChild, w= orientWeight, sk= skipRotation, name= orientConstraintName)
-	  cmds.delete(orientConstraintName)
+    # Copy Rotation Data
+    orientConstraintName = str(currentChild) + "_orientConstraint1"
+    cmds.orientConstraint( currentParent, currentChild, w= orientWeight, sk= skipRotation, name= orientConstraintName)
+    cmds.delete(orientConstraintName)
 
     if onlyParent == True:
         parentNum = 1
